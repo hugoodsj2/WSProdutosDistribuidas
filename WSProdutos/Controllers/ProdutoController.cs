2bdf0ee06fe672acf3a3126abd193979c013c6e4
Hugo Oliveira De São José <hugooliveira48@yahoo.com.br>
2015-06-01 11:04:40 -0300

    Inclusao WebService Produtos

---
+++ WSProdutos/Controllers/ProdutoController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WSProdutos.Models;
+
+namespace WSProduto.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public class AllowCrossSiteJsonAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+                base.OnActionExecuting(filterContext);
+            }
+        }
+
+        private WSProdutosContext db = new WSProdutosContext();
+
+        [HttpPost]
+        [AllowCrossSiteJson]
+        public JsonResult Listar()
+        {
+            return Json(db.Produto.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [AllowCrossSiteJson]
+        public JsonResult DetalhesProduto(string nome = "", string codigo = "")
+        {
+            return Json(db.Produto.ToList().Find(p => p.Nome.ToLower().Equals(nome.ToLower()) || p.Codigo.ToLower().Equals(codigo.ToLower())), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [AllowCrossSiteJson]
+        public JsonResult ConsultarEstoque(string codigo = "")
+        {
+            Produto prodRetorno = db.Produto.ToList().Find(p => p.Codigo.ToLower().Equals(codigo.ToLower()));
+            var retorno = new {
+                produto = prodRetorno.Nome,
+                estoque = prodRetorno.Estoque
+            };
+            return Json(retorno, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [AllowCrossSiteJson]
+        public JsonResult NovoProduto(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produto.Add(produto);
+                db.SaveChanges();
+            }
+
+            return Json(produto);
+        }
+
+        [HttpPost]
+        [AllowCrossSiteJson]
+        public JsonResult AlterarEstoque(string codigo,int estoque)
+        {
+            Produto prodEst = db.Produto.ToList().Find(prod => prod.Codigo.ToLower().Equals(codigo.ToLower()));
+            prodEst.Estoque = estoque;
+            db.SaveChanges();
+            return Json(new { produto = prodEst.Nome, novoEstoque = prodEst.Estoque });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WSProdutos/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSProdutos.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public string Categoria { get; set; }
+        public int Estoque { get; set; }
+    }
+}
